ff52a54b92b9c785abb2683e21c322d437538852
Guilherme de Oliveira <guilherme.061@gmail.com>
2021-04-25 16:30:25 -0400

    adding levels on resource locator

--- ldjam48/Assets/Scripts/ResourceLocator.cs
+++ ldjam48/Assets/Scripts/ResourceLocator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class ResourceLocator : MonoBehaviour {
     public static ResourceLocator instance;
@@ -12,6 +13,10 @@ public class ResourceLocator : MonoBehaviour {
         } else {
             Destroy(this.gameObject);
         }
+
+        m_AllLevels             = new List<string>();
+        m_AvailableLevels       = new List<string>();
+        m_AlreadyPlayedLevels   = new List<string>();
     }
 
     [Header("Monster Database")]
@@ -22,9 +27,20 @@ public class ResourceLocator : MonoBehaviour {
 
     [Header("Dungeon Settings")]
     public int CurrentDifficulty;
+    private List<string> m_AllLevels;
+    private List<string> m_AvailableLevels;
+    private List<string> m_AlreadyPlayedLevels;
 
     [Header("Player Equipments")]
     public Weapon PlayerWeapon;
+
+    private void Start() {
+        foreach(string file in System.IO.Directory.GetFiles($"{Application.dataPath}/Levels/")) {
+            if(!file.Contains(".meta")) {
+                m_AllLevels.Add(file);
+            }
+        }
+    }
 
     // TODO
     // Inventory
